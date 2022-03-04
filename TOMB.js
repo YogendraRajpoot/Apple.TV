2e00ca0f8ef1d7d8af808709cf56e378cbc15d4d
gauri9359060 <gaurip388@gmail.com>
2022-03-04 22:14:37 +0530

    first push

---
+++ TOMB.js
@@ -0,0 +1,43 @@
+const carousel = document.querySelector('.carousel');
+const slider = document.querySelector('.slider');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let direction;
+
+next.addEventListener('click', function() {
+  direction = -1;
+  carousel.style.justifyContent = 'flex-start';
+  slider.style.transform = 'translate(-20%)';  
+});
+
+prev.addEventListener('click', function() {
+  if (direction === -1) {
+    direction = 1;
+    slider.appendChild(slider.firstElementChild);
+  }
+  carousel.style.justifyContent = 'flex-end';    
+  slider.style.transform = 'translate(20%)';  
+  
+});
+
+slider.addEventListener('transitionend', function() {  
+  if (direction === 1) {
+    slider.prepend(slider.lastElementChild);
+  } else {
+    slider.appendChild(slider.firstElementChild);
+  }
+  
+  slider.style.transition = 'none';
+  slider.style.transform = 'translate(0)';
+  setTimeout(() => {
+    slider.style.transition = 'all 0.5s';
+  })
+}, false);
+
+let avatar = document.querySelector(".cast-img")
+avatar.addEventListener("click",borderChange)
+
+function borderChange(){
+    avatar.style.border = '4px solid #0288D1'
+}
+
