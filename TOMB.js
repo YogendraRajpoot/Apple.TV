75d07f695b532396540c4ec1847fb0afc73b6ed7
gauri9359060 <gaurip388@gmail.com>
2022-03-08 12:19:08 +0530

    Tragedy Of Macbeth page completed

--- TOMB.js
+++ TOMB.js
@@ -1,3 +1,5 @@
+
+    // carousel for movies
 const carousel = document.querySelector('.carousel');
 const slider = document.querySelector('.slider');
 const next = document.querySelector('.next');
@@ -41,3 +43,72 @@ function borderChange(){
     avatar.style.border = '4px solid #0288D1'
 }
 
+
+
+        //Movie Modal
+function viewModal(){
+  let modal = document.getElementById("myModal");
+    console.log("clicked")
+    modal.style.display = "block"
+}
+
+function hideModal(){
+  let modal = document.getElementById("myModal")
+  modal.style.display = "none"
+}
+
+
+
+
+        
+
+    // Share Button
+function viewShareBtn(){
+    var share = document.querySelector(".share")
+    share.style.visibility ="visible"
+    console.log("clicked")
+}
+
+function hideContent(){
+    var share = document.querySelector(".share")
+    share.style.visibility ="hidden"
+    console.log("clicked2")
+}
+
+
+// Carousel for Cast and Crew
+const castCarousel = document.querySelector('.cast-carousel');
+const castSlider = document.querySelector('.cast-slider');
+const castNext = document.querySelector('.cast-next');
+const castPrev = document.querySelector('.cast-prev');
+let flag;
+
+castNext.addEventListener('click', function() {
+  flag = -1;
+  castCarousel.style.justifyContent = 'flex-start';
+  castSlider.style.transform = 'translate(-20%)';  
+});
+
+castPrev.addEventListener('click', function() {
+  if (flag === -1) {
+    flag = 1;
+    castSlider.appendChild(castSlider.firstElementChild);
+  }
+  castCarousel.style.justifyContent = 'flex-end';    
+  castSlider.style.transform = 'translate(20%)';  
+  
+});
+
+castSlider.addEventListener('transitionend', function() {  
+  if (flag === 1) {
+    castSlider.prepend(castSlider.lastElementChild);
+  } else {
+    castSlider.appendChild(castSlider.firstElementChild);
+  }
+  
+  castSlider.style.transition = 'none';
+  castSlider.style.transform = 'translate(0)';
+  setTimeout(() => {
+    castSlider.style.transition = 'all 0.5s';
+  })
+}, false);
